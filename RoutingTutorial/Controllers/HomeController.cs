a5dca2800d6eebabb2d513e2f8041f2eda0b707d
Frank Eslami <eslamifrank@gmail.com>
2020-10-03 13:47:02 -0700

    Demo completed.

---
+++ RoutingTutorial/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoutingTutorial.Controllers {
+    public class HomeController: Controller {
+        [Route("/")]
+        public IActionResult Index() {
+            return Content("Home");
+        }
+
+        [Route("About")]
+        public IActionResult About() {
+            return Content("About");
+        }
+
+        public IActionResult Display(string id) {
+            if (id == null)
+                return Content("No ID supplied");
+            else
+                return Content("Id is " + id);
+        }
+
+        [Route("[action]/{start}/{end?}/{message?}")]
+        public IActionResult Countdown(int start, int end = 0, string message = "") {
+            string contentString = "Counting down:\n";
+            for (int i = start; i >= end; i--) {
+                contentString += i + "\n";
+            }
+            contentString += message;
+            return Content(contentString);
+        }
+    }
+}
---
+++ RoutingTutorial/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingTutorial.Controllers {
+    public class ProductController : Controller {
+        public IActionResult List(string cat, int num) {
+            return Content("Product Controller, List Action, " +
+                    "Category = " + cat + ", Page = " + num);
+        }
+
+        public IActionResult Detail(int id) {
+            return Content("Product Controller, Detail Action, ID = " + id);
+        }
+    }
+}
